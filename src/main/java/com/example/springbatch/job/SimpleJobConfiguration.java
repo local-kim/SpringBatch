1ee3b8f593bc3bcd01f55d55b9176a93893c0bd7
Kim Hyunji <hzunhy@gmail.com>
2024-02-05 13:24:12 +0900

    Job, Step1 실패, Step2

--- src/main/java/com/example/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springbatch/job/SimpleJobConfiguration.java
@@ -31,6 +31,7 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")   // simpleJob 이란 이름의 Batch Job을 생성합니다. job의 이름은 별도로 지정하지 않고, 이렇게 Builder를 통해 지정합니다.
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -43,11 +44,20 @@ public class SimpleJobConfiguration {
     public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")    // simpleStep1 이란 이름의 Batch Step을 생성합니다. 마찬가지로 Builder를 통해 이름을 지정합니다.
                 .tasklet((contribution, chunkContext) -> {  // Step 안에서 수행될 기능들을 명시합니다. Tasklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언할 때 사용합니다.
-                    log.info(">>>>> This is Step1");    // Batch가 수행되면 log가 출력되도록 합니다.
+                    throw new IllegalArgumentException("step1에서 실패합니다.");
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
                     log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
-
 }
